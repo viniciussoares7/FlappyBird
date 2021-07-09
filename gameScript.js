a75b00508b185746d23c01cbf7ccdbf0ffda51bd
viniciussoares7 <vinicius-a-soares@outlook.com>
2021-07-08 22:12:48 -0300

    added sounds && floor colision effects

--- gameScript.js
+++ gameScript.js
@@ -1,83 +1,13 @@
 console.log('Flappy Bird - Vinicius')
 
+const sound_hit = new Audio()
+sound_hit.src = './assets/sound_efects/hit.wav'
+
 const sprites = new Image()
-sprites.src = './assets/sprites.png'
+sprites.src = './assets/images/sprites.png'
 
 const canvas = document.querySelector('canvas')
 const contexto = canvas.getContext('2d')
-
-// bird
-
-const flappyBird = {
-  sx: 0,
-  sy: 0,
-  sWidth: 34,
-  sHeight: 24,
-  dx: 10,
-  dy: 50,
-  dWidth: 34,
-  dHeight: 24,
-  falldownSpeed: 0,
-  gravity: 0.25,
-
-  refresh() {
-    flappyBird.falldownSpeed = flappyBird.falldownSpeed + flappyBird.gravity
-    flappyBird.dy = flappyBird.dy + flappyBird.falldownSpeed
-    console.log(flappyBird.falldownSpeed)
-  },
-
-  print() {
-    contexto.drawImage(
-      sprites,
-      flappyBird.sx,
-      flappyBird.sy,
-      flappyBird.sWidth,
-      flappyBird.sHeight,
-      flappyBird.dx,
-      flappyBird.dy,
-      flappyBird.dWidth,
-      flappyBird.dHeight
-    )
-  }
-}
-
-// floor (chão)
-
-const floor = {
-  sx: 0,
-  sy: 610,
-  sWidth: 224,
-  sHeight: 112,
-  dx: 0,
-  dy: canvas.height - 112,
-  dWidth: 224,
-  dHeight: 112,
-
-  print() {
-    contexto.drawImage(
-      sprites,
-      floor.sx,
-      floor.sy,
-      floor.sWidth,
-      floor.sHeight,
-      floor.dx,
-      floor.dy,
-      floor.dWidth,
-      floor.dHeight
-    )
-    contexto.drawImage(
-      sprites,
-      floor.sx,
-      floor.sy,
-      floor.sWidth,
-      floor.sHeight,
-      floor.dx + floor.sWidth,
-      floor.dy,
-      floor.dWidth,
-      floor.dHeight
-    )
-  }
-}
 
 // background (fundo)
 
@@ -120,6 +50,44 @@ const background = {
   }
 }
 
+// floor (chão)
+
+const floor = {
+  sx: 0,
+  sy: 610,
+  sWidth: 224,
+  sHeight: 112,
+  dx: 0,
+  dy: canvas.height - 112,
+  dWidth: 224,
+  dHeight: 112,
+
+  print() {
+    contexto.drawImage(
+      sprites,
+      floor.sx,
+      floor.sy,
+      floor.sWidth,
+      floor.sHeight,
+      floor.dx,
+      floor.dy,
+      floor.dWidth,
+      floor.dHeight
+    )
+    contexto.drawImage(
+      sprites,
+      floor.sx,
+      floor.sy,
+      floor.sWidth,
+      floor.sHeight,
+      floor.dx + floor.sWidth,
+      floor.dy,
+      floor.dWidth,
+      floor.dHeight
+    )
+  }
+}
+
 //start msg
 
 const mensageGetReady = {
@@ -147,19 +115,88 @@ const mensageGetReady = {
   }
 }
 
+// bird
+function createFlappyBird() {
+  const flappyBird = {
+    sx: 0,
+    sy: 0,
+    sWidth: 34,
+    sHeight: 24,
+    dx: 10,
+    dy: 50,
+    dWidth: 34,
+    dHeight: 24,
+    falldownSpeed: 0,
+    gravity: 0.25,
+    jump: 4.6,
+    action() {
+      flappyBird.falldownSpeed = -flappyBird.jump
+    },
+
+    refresh() {
+      if (doCollision(flappyBird, floor)) {
+        console.log('hit')
+        sound_hit.play()
+
+        setTimeout(() => {
+          screenChange(screens.startScreen)
+        }, 300)
+        return
+      }
+
+      flappyBird.falldownSpeed = flappyBird.falldownSpeed + flappyBird.gravity
+      flappyBird.dy = flappyBird.dy + flappyBird.falldownSpeed
+    },
+
+    print() {
+      contexto.drawImage(
+        sprites,
+        flappyBird.sx,
+        flappyBird.sy,
+        flappyBird.sWidth,
+        flappyBird.sHeight,
+        flappyBird.dx,
+        flappyBird.dy,
+        flappyBird.dWidth,
+        flappyBird.dHeight
+      )
+    }
+  }
+  return flappyBird
+}
+
+function doCollision(flappyBird, floor) {
+  const flappyHeight = flappyBird.dy + flappyBird.sHeight
+  const floorHeight = floor.dy
+
+  if (flappyHeight >= floorHeight) {
+    return true
+  }
+  return false
+}
+
 //screens
+const global = {}
+
 let activeScreen = {}
 
 function screenChange(newScreen) {
   activeScreen = newScreen
+
+  if (activeScreen.ini) {
+    activeScreen.ini()
+  }
 }
 
 const screens = {
   startScreen: {
+    ini() {
+      global.flappyBird = createFlappyBird()
+    },
     print() {
       background.print()
       floor.print()
-      flappyBird.print()
+      global.flappyBird.print()
       mensageGetReady.print()
     },
     click() {
@@ -172,10 +209,13 @@ screens.game = {
   print() {
     background.print()
     floor.print()
-    flappyBird.print()
+    global.flappyBird.print()
+  },
+  click() {
+    global.flappyBird.action()
   },
   refresh() {
-    flappyBird.refresh()
+    global.flappyBird.refresh()
   }
 }
 
